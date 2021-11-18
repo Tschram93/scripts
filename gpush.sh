a98da7e19507555f486f40620247a9d00e75c70a
TS93 <tschram93@yahoo.com>
2021-11-18 00:00:42 -0500

    Adding automation to github add, commit, and push

--- gpush.sh
+++ gpush.sh
@@ -6,7 +6,7 @@ repo=$1
 file=$2
 
 if [ -z "$repo" ] || [ -z "$file" ]; then
-    echo ERROR: Please include repo name followed by filename or \* \(glob\) for entire dir.
+    echo ERROR: Enter repo name and filename \* \* \  for entire directory.
     exit 1
 fi
 
@@ -18,12 +18,12 @@ read -p "Would you like to push changes?: " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
         git add .
     elif [[ $REPLY =~ ^[Nn]$ ]]; then
-        printf "\nNo Changes Pushed"
+        printf "\n No Changes Pushed"
         sleep 2
         exit
     fi
 
-printf "\nadd message\n"
+printf "\n add message \n"
 read -r message
 
     git commit -m "$message"
